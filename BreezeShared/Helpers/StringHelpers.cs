014bfbb439f7365fb103d594f8a34aa49900fd69
James@ezmuze.co.uk <James@ezmuze.co.uk>
2018-08-13 21:25:27 +0100

    Added missing file

---
+++ BreezeShared/Helpers/StringHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breeze.Helpers
+{
+    public static class StringHelpers
+    {
+        public static List<string> ToList(this string input)
+        {
+            return input.Split('\n').Select(y => y.Replace("\r", "")).ToList();
+        }
+
+
+    }
+}
